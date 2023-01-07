b6ea5cf74f9686d423fa332ec522a9564b709712
zhu_yuquan <zhu_yuquan6@163.com>
2023-01-07 22:08:57 +0800

    1

--- wechat-ui/src/views/search/index.vue
+++ wechat-ui/src/views/search/index.vue
@@ -1,20 +1,37 @@
 <template>
-  <div>
-    <van-empty :description="msg" />
-  </div>
+    <div>
+        <van-empty :description="msg"/>
+    </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      msg: "搜索"
-    };
-  },
-  created(){
+    import {pernovelList,getNovelDetail} from "@/api/pernovel/pernovel";
 
-  },
-  methods: {
-  }
-}
+    export default {
+        data() {
+            return {
+                msg: "搜索",
+                novelList: []
+
+            };
+        },
+        created() {
+            this.getNovelList(1)
+        },
+        methods: {
+            getNovelList(){
+                pernovelList({pageSize: 10,pageNum:1}).then(res => {
+                    console.log("res：" + res)
+                    if (res.code == 200){
+
+                    } else {
+                        return Promise.reject(new Error(res.msg))
+                    }
+                    this.loading = false;
+                })
+            }
+
+
+        }
+    }
 </script>
